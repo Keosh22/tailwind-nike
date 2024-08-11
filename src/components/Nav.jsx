db2342ad9bfc58bfdba96d3ae34d4e3bde544a05
Ken Joshua Buenavides <kenjoshuabuenavides@gmail.com>
2024-08-12 00:00:21 +0700

    navbar done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,24 +1,47 @@
 import { headerLogo } from "../assets/images";
 import { hamburger } from "../assets/icons";
-import { navLinks  } from "../constants";
+import { navButtons, navLinks  } from "../constants";
 import { list } from "postcss";
 
 const Nav = () => {
   return (
     <header className="padding-x py-8 absolute z-10 w-full">
+
+      {/* Logo */}
       <nav className="flex justify-between items-center max-container">
         <a href="/">
           <img src={headerLogo} alt="" />
         </a>
+
+        {/* Links */}
           <ul className="flex flex-1 justify-center items-center gap-16 max-lg:hidden">
             {navLinks.map((item) => (
               <li key={item.label}>
-                <a href={item.href} className="font-montserrat text-lg leading-normal text-slate-gray">
+                <a href={item.href} className="font-montserrat text-lg leading-normal text-slate-gray hover:text-black transition ease-in-out duration-300">
                   {item.label}
                 </a>
               </li>
             ))}
           </ul>
+
+          {/* Buttons */}
+          <ul className="flex justify-center items-center gap-6 max-lg:hidden">
+            {navButtons.map((item) => (
+              <li><a href={item.href} className="text-lg font-montserrat leading-normal hover:text-slate-gray transition ease-in-out duration-300">{item.label}</a></li>
+            ))}
+          </ul>
+
+          {/* Hamburger Menu */}
+            <div className="hidden max-lg:block">
+              <img 
+              src={hamburger} 
+              alt="Menu" 
+              width={25}
+              height={25}
+              />
+            </div>
+
+         
       </nav>
     </header>
   )
