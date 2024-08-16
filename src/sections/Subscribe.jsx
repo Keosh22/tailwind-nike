f7de7aefcac77b1c8bb82b2a55e8a6ec36b52134
Ken Joshua Buenavides <kenjoshuabuenavides@gmail.com>
2024-08-16 16:01:11 +0700

    Fix bug change the button text sign up to register

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -6,7 +6,7 @@ const Subscribe = () => {
       <div className="lg:max-w-[40%] w-full flex items-center gap-5 p-2.5 max-sm:flex-col sm:border sm:border-slate-gray rounded-full">
         <input type="text" placeholder="subscibe@nike.com" className="input" />
         <div className="flex items-center max-sm:w-full max-sm:justify-end">
-          <button className="bg-coral-red text-white py-4 px-7 rounded-full font-montserrat text-lg leading-normal border border-coral-red flex justify-center items-center gap-2 w-full">Sign up</button>
+          <button className="bg-coral-red text-white py-4 px-7 rounded-full font-montserrat text-lg leading-normal border border-coral-red flex justify-center items-center gap-2 w-full">Register</button>
         </div>
       </div>
     </section>
